b3e872789b6092c603092e48c50e901783cb2a77
aferri <alessandro.ferri@business-changers.it>
2022-05-23 10:38:15 +0200

    Add Mongoose

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const recensioniRouter = require('./api/routes/recensioni');
+const mongoose = require('mongoose')
+const cors = require('cors');
+
+mongoose.connect('mongodb+srv://aferri:' + process.env.MONGO_ATLAS_PW + '@node-recensioni.esflg.mongodb.net/?retryWrites=true&w=majority');
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-})
+app.use(cors());
 
 app.use('/recensioni', recensioniRouter);
 
